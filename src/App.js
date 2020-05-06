2341ebf7a58065ec5f6bcde50395610ca8c3c794
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2020-05-05 23:39:11 -0400

    get basics down

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [user, setUser] = useState(null)
+    const [userId, setUserId] = useState(1);
+
+    const getUser = async (id) => {
+        const user = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`).then(response => response.json())
+        console.log('user:', user)
+        setUser(user);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        getUser(userId);
+    }
+
+    const handleChange = (e) => {
+        const id = e.target.value
+        if (id < 11) setUserId(id);
+    }
+
+    useEffect(() => {
+        getUser(1);
+    }, [])
+
+    const userCard = () => {
+        return (
+            <>
+                <h1>{user.name}</h1>
+                <h2>{user.email}</h2>
+                <p>{user.username}</p>
+            </>
+        );
+    }
+    return (
+        <div className="App">
+            <h1>React Testing Example</h1>
+            {
+                user
+                    ? userCard()
+                    : <h1>Enter an id</h1>
+            }
+            <form onSubmit={handleSubmit} >
+                <label htmlFor='user-id'>User Id</label>
+                <input id='user-id' value={userId} onChange={handleChange}></input>
+                <input type='submit' value='Submit' />
+            </form>
+        </div>
+    );
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,42 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { enableFetchMocks } from 'jest-fetch-mock'
+import { render, waitForElement, getByLabelText } from '@testing-library/react';
 import App from './App';
+enableFetchMocks();
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Basic Test', () => {
+    // afterEach(cleanup); // this step is no longer needed
+
+    const setup = async () => {
+        const thing = JSON.stringify({
+            address: 'whatever',
+            email: "Sincere@april.biz",
+            id: 1,
+            name: "Leanne Graham",
+            phone: "1-770-736-8031 x56442",
+            username: "Bret",
+            website: "hildegard.org",
+        });
+
+        fetch.mockOnce(thing);
+        const utils = render(<App />);
+
+        await waitForElement(() => utils.getByText(/Leanne Graham/))
+        return {
+            ...utils,
+        }
+    }
+
+    it('renders at al', async () => {
+        const { debug, getByText, inputId } = await setup();
+        const mainHeader = getByText(/React Testing Example/i);
+        expect(mainHeader).toBeInTheDocument();
+    });
+
+    it('Grabs first user', async () => {
+        const { debug, getByText } = await setup();
+        const linkElement = getByText(/Leanne Graham/i);
+        expect(linkElement).toBeInTheDocument();
+        debug();
+    });
 });
